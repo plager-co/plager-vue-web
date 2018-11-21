8e0a888884c9d343b18a4a44e7e6baeb4dcdc2a5
Unknown <uniqcon89@gmail.com>
2018-11-21 22:02:02 +0900

    deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8083'
+const API_URL = 'http://api.plager.kr'
 
 function extend(target) {
     var sources = [].slice.call(arguments, 1);

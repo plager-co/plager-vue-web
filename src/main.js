b042e0dee43be4ca28a9ba5f772e20ba6d795838
Unknown <uniqcon89@gmail.com>
2018-12-01 22:11:27 +0900

    temp instagram login

--- src/main.js
+++ src/main.js
@@ -7,10 +7,25 @@ import { store } from './store'
 import VueCarousel from 'vue-carousel'
 import Meta from 'vue-meta'
 import VueClipboard from 'vue-clipboard2'
+import VueAuthenticate from 'vue-authenticate'
+import VueAxios from 'vue-axios'
+import axios from 'axios';
+
+Vue.use(VueAxios, axios)
 
 Vue.use(VueClipboard)
 Vue.use(VueCarousel)
 Vue.use(Meta)
+Vue.use(VueAuthenticate, {
+  baseUrl: 'http://127.0.0.1:8083', // Your API domain
+
+  providers: {
+    instagram: {
+      clientId: 'd350de41c32e4c47b97061fa9747d367',
+      redirectUri: 'http://127.0.0.1:8080/influencer-my-page' // Your client app URL
+    }
+  }
+})
 Vue.config.productionTip = false
 
 import inViewportDirective from 'vue-in-viewport-directive'

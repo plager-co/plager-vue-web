08f5beeabaef136d1adb0816fb8e3fd86172f079
Unknown <uniqcon89@gmail.com>
2018-12-23 13:45:22 +0100

    try

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill';
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -10,7 +11,10 @@ import VueClipboard from 'vue-clipboard2'
 import VueAuthenticate from 'vue-authenticate'
 import VueAxios from 'vue-axios'
 import axios from 'axios';
+<<<<<<< HEAD
 require('es6-promise').polyfill()
+=======
+>>>>>>> 35126fe... try
 Vue.use(VueAxios, axios)
 
 Vue.use(VueClipboard)
